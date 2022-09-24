dc771337e765cfccb32adbf889335f29738f346f
ArinSwagg <rakuzanoat@gmail.com>
2022-09-24 14:36:25 +0700

    change directory

--- resources/views/dashboard_admin.blade.php
+++ resources/views/dashboard_admin.blade.php
@@ -22,7 +22,7 @@
     <x-app-layout>
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Admin Management TESTTESTETSTES') }}
+                {{ __('Admin Management') }}
             </h2>
         </x-slot>
         <div class="container mt-5 py-3 border admin-manage">
